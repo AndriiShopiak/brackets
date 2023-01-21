0f6232bc4f5dba19b35cd8a31bbdb65ad57cbe45
Andrii Shopiak <andriyshopyak@gmail.com>
2023-01-21 17:36:53 +0200

    feat: add solution for of

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let stack = [];
+
+  let BracketsConfigObj = new Map(bracketsConfig);
+  let strSplit = str.split('');
+  for ( let ch of strSplit) {
+    if (ch == BracketsConfigObj.get(stack[stack.length -1])) {
+      stack.pop();
+    } else {
+      stack.push(ch);
+    }
+  }
+  if (stack.length == 0) {
+    return true
+  } else {
+    return false
+  }
 }
